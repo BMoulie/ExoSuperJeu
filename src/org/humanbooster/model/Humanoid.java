7e3f9ac5a942c051af1e62ff3acd35611f374b51
BMoulie <bertrand.moulie@outlook.fr>
2022-04-06 12:37:17 +0200

    Implémentation de l'ajout d'éléments pour les listes de la classe Humanoid.

--- src/org/humanbooster/model/Humanoid.java
+++ src/org/humanbooster/model/Humanoid.java
@@ -7,6 +7,14 @@ public abstract class Humanoid extends Being {
     protected ArrayList<Animal> animals = new ArrayList<>();
     protected ArrayList<House> houses = new ArrayList<>();
 
+    public void addAnimal(Animal a) {
+        this.animals.add(a);
+    }
+
+    public void addHouse(House h) {
+        this.houses.add(h);
+    }
+
     public Humanoid(String name, int x, int y, int maxHealth, int health, double resistance) {
         super(name, x, y, maxHealth, health, resistance);
     }
